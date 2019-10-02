a2ee39e7a0c79ab50d8ad612a970be8ac291188c
PythonProjectDeveloper <SleepyDevil@protonmail.com>
2019-10-02 12:20:32 +0300

    feat: add journal constants

---
+++ src/app/common/constants/constants-journal.ts
@@ -0,0 +1,744 @@
+import { Journal } from '../entities/journal';
+
+export const journals: Journal[] = [
+  {
+    subjectId: "5d92e08d2168f62bce92241e",
+    days: [
+      {
+        name: "04/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: 2
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 1
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 1
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 2
+          }
+        ]
+      },
+      {
+        name: "05/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 5
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 3
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: 2
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 5
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 3
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 5
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 2
+          }
+        ]
+      },
+      {
+        name: "06/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: null
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 2
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: null
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 2
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 4
+          }
+        ]
+      },
+      {
+        name: "07/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: null
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 5
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: null
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: null
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 3
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: null
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: null
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 5
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: null
+          }
+        ]
+      }
+    ]
+  },
+  {
+    subjectId: "5d92e08de9c11a2c40afa03e",
+    days: [
+      {
+        name: "04/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: 2
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 1
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 1
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 2
+          }
+        ]
+      },
+      {
+        name: "05/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 5
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 3
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: 2
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 5
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 3
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 5
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 2
+          }
+        ]
+      },
+      {
+        name: "06/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: null
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 2
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: null
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 2
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 4
+          }
+        ]
+      },
+      {
+        name: "07/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: null
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 5
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: null
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: null
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 3
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: null
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: null
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 5
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: null
+          }
+        ]
+      }
+    ]
+  },
+  {
+    subjectId: "5d92e08d8257e8d239ccb7fc",
+    days: [
+      {
+        name: "04/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: 2
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 1
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 1
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 2
+          }
+        ]
+      },
+      {
+        name: "05/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 5
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 3
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: 2
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 5
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 3
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 5
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 2
+          }
+        ]
+      },
+      {
+        name: "06/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: null
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 2
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: null
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 2
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 4
+          }
+        ]
+      },
+      {
+        name: "07/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: null
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 5
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: null
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: null
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 3
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: null
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: null
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 5
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: null
+          }
+        ]
+      }
+    ]
+  },
+  {
+    subjectId: "5d92e08d25068063e77d2fab",
+    days: [
+      {
+        name: "04/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: 2
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 1
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 1
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 1
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 2
+          }
+        ]
+      },
+      {
+        name: "05/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 5
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 3
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: 2
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 5
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 3
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 5
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 2
+          }
+        ]
+      },
+      {
+        name: "06/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: null
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 2
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 2
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: null
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 2
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: 4
+          }
+        ]
+      },
+      {
+        name: "07/02",
+        marks: [
+          {
+            studentId: "5d92dde4eb2e470b17ba8994",
+            value: null
+          },
+          {
+            studentId: "5d92dde4bb3d073dd3249a63",
+            value: 5
+          },
+          {
+            studentId: "5d92dde4de5533607bc97d6a",
+            value: null
+          },
+          {
+            studentId: "5d92dde4b15d989e9caf6e4c",
+            value: null
+          },
+          {
+            studentId: "5d92dde456c8eb135b61b1ed",
+            value: 3
+          },
+          {
+            studentId: "5d92dde42d994d3aba48f851",
+            value: null
+          },
+          {
+            studentId: "5d92dde4c510b89eaea10036",
+            value: 4
+          },
+          {
+            studentId: "5d92dde4cb1b2aee443a3ec5",
+            value: null
+          },
+          {
+            studentId: "5d92dde4f57ab520e345f7db",
+            value: 5
+          },
+          {
+            studentId: "5d92dde472c93333e2b95278",
+            value: null
+          }
+        ]
+      }
+    ]
+  }
+]
