9711106e404ff3f29dd78b720c44ee55993f250b
PythonProjectDeveloper <SleepyDevil@protonmail.com>
2019-10-14 17:47:20 +0300

    refactor: remove comments

--- src/app/common/guards/exit-about.guard.ts
+++ src/app/common/guards/exit-about.guard.ts
@@ -16,14 +16,9 @@ export class ExitAboutGuard implements CanDeactivate<ComponentCanDeactivate> {
     private leaveGuardModalWindowService: LeaveGuardModalWindowService
   ) { }
 
-  // public canDeactivate(component: ComponentCanDeactivate): Observable<boolean> | boolean {
-  //     const isDeactivated: Observable<boolean> | boolean = component.canDeactivate ? component.canDeactivate() : true;
-  //     return isDeactivated || confirm('Вы хотите покинуть страницу без сохранения данных?');
-  // }
-
   public canDeactivate(component: ComponentCanDeactivate): Observable<boolean> | boolean {
     const isDeactivated: Observable<boolean> | boolean = component.canDeactivate ? component.canDeactivate() : true;
     return isDeactivated || this.leaveGuardModalWindowService.showModalWindow();
-}
+  }
 
 }
