226bd5d891ed647a01594fa9880b69347a069369
PythonProjectDeveloper <SleepyDevil@protonmail.com>
2019-10-22 17:06:21 +0300

    backup: dropdown files

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
-import { MatIconModule, MatButtonModule, MatCardModule, MatFormField, MatFormFieldModule, MatInputModule } from '@angular/material';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 import { RouterModule } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslateLanguageSettings } from '../common/helpers/factories';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
+import {
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatCheckboxModule
+} from '@angular/material';
 
 @NgModule({
-  declarations: [ButtonComponent, PageNotFoundComponent, BreadcrumbComponent, ToolbarComponent, DropdownComponent],
+  declarations: [
+    ButtonComponent,
+    PageNotFoundComponent,
+    BreadcrumbComponent,
+    ToolbarComponent,
+    DropdownComponent
+  ],
   imports: [
     CommonModule,
     MatButtonModule,
@@ -25,8 +39,18 @@ import { DropdownComponent } from './components/dropdown/dropdown.component';
     MatInputModule,
     FormsModule,
     HttpClientModule,
+    MatExpansionModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
     TranslateModule.forChild(TranslateLanguageSettings())
   ],
-  exports: [ButtonComponent, PageNotFoundComponent, BreadcrumbComponent, ToolbarComponent, DropdownComponent, TranslateModule]
+  exports: [
+    ButtonComponent,
+    PageNotFoundComponent,
+    BreadcrumbComponent,
+    ToolbarComponent,
+    DropdownComponent,
+    TranslateModule
+  ]
 })
 export class SharedModule { }
