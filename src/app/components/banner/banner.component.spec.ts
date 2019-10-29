a489881ea0db2660d5fc491964f13e9c02f02ae0
PythonProjectDeveloper <SleepyDevil@protonmail.com>
2019-10-29 18:34:53 +0300

    feat: add several tests

--- src/app/components/banner/banner.component.spec.ts
+++ src/app/components/banner/banner.component.spec.ts
@@ -1,25 +1,121 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { BannerComponent } from './banner.component';
+import { BannerComponent } from './banner.component';
+import { MatIconModule } from '@angular/material';
+import { BannerService } from 'src/app/common/services/banner.service';
+import { Observable, of } from 'rxjs';
+import { SimpleMessageComponent } from './simple-message/simple-message.component';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { BannerDirective } from 'src/app/common/directives/banner.directive';
 
-// describe('BannerComponent', () => {
-//   let component: BannerComponent;
-//   let fixture: ComponentFixture<BannerComponent>;
+let component: BannerComponent;
+let fixture: ComponentFixture<BannerComponent>;
+let data: string = 'message';
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ BannerComponent ]
-//     })
-//     .compileComponents();
-//   }));
+class BannerServiceStub {
+  public getBannerStatus(): Observable<boolean> {
+    return of(true);
+  }
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(BannerComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  public getBunner(): Observable<string> {
+    return of(data);
+  }
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  public setBannerStatus(flag: boolean): void { }
+}
+
+function configureTestingModule(): void {
+  TestBed.configureTestingModule({
+    declarations: [
+      BannerComponent,
+      SimpleMessageComponent,
+      BannerDirective
+    ],
+    imports: [
+      MatIconModule
+    ],
+    providers: [
+      { provide: BannerService, useClass: BannerServiceStub }
+    ]
+  }).overrideModule(BrowserDynamicTestingModule, {
+    set: {
+      entryComponents: [ SimpleMessageComponent ],
+    }
+  });
+}
+
+function setComponent(): void {
+  fixture = TestBed.createComponent(BannerComponent);
+  component = fixture.componentInstance;
+  fixture.detectChanges();
+}
+
+describe('BannerComponent', () => {
+  describe('#ngOnInit', () => {
+    beforeEach(async(configureTestingModule));
+    beforeEach(setComponent);
+
+    it('should set banner status', () => {
+      component.isOpen = false;
+
+      component.ngOnInit();
+
+      expect(component.isOpen).toEqual(true);
+    });
+
+    it('should call loadComponent with getting data', () => {
+      spyOn(component, 'loadComponent');
+
+      component.ngOnInit();
+
+      expect(component.loadComponent).toHaveBeenCalledWith(data);
+    });
+  });
+
+  describe('#loadComponent', () => {
+    beforeEach(async(configureTestingModule));
+    beforeEach(setComponent);
+
+    it('should create banner and add him to dom', () => {
+      component.loadComponent(data);
+
+      fixture.detectChanges();
+
+      const banner: any = fixture.nativeElement.querySelector('.banner');
+      const message: string = banner.querySelector('p').innerText;
+
+      expect(banner).toBeDefined();
+      expect(message).toEqual(data);
+    });
+  });
+
+  describe('#onClose', () => {
+    beforeEach(async(configureTestingModule));
+    beforeEach(setComponent);
+
+    it('should call onClose when button click', () => {
+      const button: any = fixture.nativeElement.querySelector('.button');
+
+      spyOn(component, 'onClose');
+
+      button.dispatchEvent(new Event('click'));
+
+      fixture.detectChanges();
+
+      expect(component.onClose).toHaveBeenCalled();
+    });
+
+    it('should set bunner status', () => {
+      const service: any = TestBed.get(BannerService);
+
+      spyOn(service, 'setBannerStatus');
+
+      const button: any = fixture.nativeElement.querySelector('.button');
+      button.dispatchEvent(new Event('click'));
+
+      fixture.detectChanges();
+
+      expect(service.setBannerStatus).toHaveBeenCalledWith(false);
+    });
+  });
+});
--- src/app/components/students/student-page/student-page.component.spec.ts
+++ src/app/components/students/student-page/student-page.component.spec.ts
@@ -1,25 +1,29 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { StudentPageComponent } from './student-page.component';
+import { StudentPageComponent } from './student-page.component';
+import { RouterModule } from '@angular/router';
 
-// describe('StudentPageComponent', () => {
-//   let component: StudentPageComponent;
-//   let fixture: ComponentFixture<StudentPageComponent>;
+describe('StudentPageComponent', () => {
+  let component: StudentPageComponent;
+  let fixture: ComponentFixture<StudentPageComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ StudentPageComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StudentPageComponent ],
+      imports: [
+        RouterModule.forRoot([])
+      ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(StudentPageComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StudentPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
